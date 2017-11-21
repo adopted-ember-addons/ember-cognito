eafb7f498483e2525c01fdce871c618762d0f508
Paul Watts <paulcwatts@gmail.com>
2017-11-21 11:45:29 -0800

    Update eslint-plugin-ember, fix new issues
    Begin removing jQuery, and fix another issue with the dummy app.
    Also, bump ember-cli-update.

--- tests/dummy/app/controllers/profile.js
+++ tests/dummy/app/controllers/profile.js
@@ -1,12 +1,16 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default Controller.extend({
   currentUser: service(),
-  attributes: [
-    'sub',
-    'username',
-    'email',
-    'email_verified'
-  ]
+  init() {
+    this._super(...arguments);
+    set(this, 'attributes', [
+      'sub',
+      'username',
+      'email',
+      'email_verified'
+    ]);
+  }
 });
