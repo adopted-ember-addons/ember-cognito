67bad2f57978b3620dfdf581c402b6db62aefb60
Paul Watts <paulcwatts@gmail.com>
2018-01-09 13:41:09 -0800

    Add sign up method
    As always, there's a lot more rigamarole to stub methods so we
    can actually test these.

--- addon/services/cognito.js
+++ addon/services/cognito.js
@@ -1,8 +1,45 @@
 import Service from '@ember/service';
+import { Promise } from 'rsvp';
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+import CognitoStorage from '../utils/cognito-storage';
+import CognitoUser from '../utils/cognito-user';
 
 /**
  * @public
- * This is a container for easily accessing the logged-in CognitoUser object.
+ * This is a container for easily accessing the logged-in CognitoUser object,
+ * as well as creating others using signUp().
  */
 export default Service.extend({
+  // Primarily used so we can stub methods.
+  _stubPool(pool) {
+    return pool;
+  },
+
+  /**
+   * Method for signing up a user.
+   *
+   * @param username User's username
+   * @param password Plain-text initial password entered by user.
+   * @param attributeList New user attributes.
+   * @param validationData Application metadata.
+   */
+  signUp(username, password, attributeList, validationData) {
+    let { poolId, clientId } = this.getProperties('poolId', 'clientId');
+    let pool = this._stubPool(new CognitoUserPool({
+      UserPoolId: poolId,
+      ClientId: clientId,
+      Storage: new CognitoStorage({})
+    }));
+
+    return new Promise((resolve, reject) => {
+      pool.signUp(username, password, attributeList, validationData, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          result.user = CognitoUser.create({ user: result.user });
+          resolve(result);
+        }
+      });
+    }, `cognito-service#signUp`);
+  }
 });
--- tests/unit/services/cognito-test.js
+++ tests/unit/services/cognito-test.js
@@ -1,11 +1,68 @@
 import { get } from '@ember/object';
 import { moduleFor, test } from 'ember-qunit';
+import sinonTest from 'ember-sinon-qunit/test-support/test';
+import { CognitoUser as AWSCognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js';
+import CognitoUser from 'dummy/utils/cognito-user';
 
 moduleFor('service:cognito', 'Unit | Service | cognito', {
+  beforeEach() {
+    this.stubPoolMethod = function(service, method, fn) {
+      this.stub(service, '_stubPool').callsFake((pool) => {
+        this.stub(pool, method).callsFake(fn);
+        return pool;
+      });
+    };
+  }
 });
+
+function getAwsUser(username) {
+  let pool = new CognitoUserPool({ UserPoolId: 'us-east-1_TEST', ClientId: 'TEST' });
+  return new AWSCognitoUser({ Username: username, Pool: pool });
+}
 
 test('config is set correctly', function(assert) {
   let service = this.subject();
   assert.equal(get(service, 'poolId'), 'us-east-1_TEST');
   assert.equal(get(service, 'clientId'), 'TEST');
+});
+
+sinonTest('signup works', function(assert) {
+  let service = this.subject();
+  this.stubPoolMethod(service, 'signUp', (username, password, attributeList, validationData, callback) => {
+    // Return data is:
+    // user: new CognitoUser
+    // userConfirmed: boolean
+    // userSub: ID
+    callback(null, {
+      user: getAwsUser(username),
+      userConfirmed: true,
+      userSub: 'xxxx'
+    });
+  });
+
+  return service.signUp('testuser', 'password', [], null).then((result) => {
+    // The user should be upgraded to one of our users
+    assert.ok(result.user instanceof CognitoUser);
+    assert.equal(result.userConfirmed, true);
+    assert.equal(result.userSub, 'xxxx');
+  });
+});
+
+sinonTest('signup error', function(assert) {
+  assert.expect(1);
+
+  let service = this.subject();
+  this.stubPoolMethod(service, 'signUp', (username, password, attributeList, validationData, callback) => {
+    // Return data is:
+    // user: new CognitoUser
+    // userConfirmed: boolean
+    // userSub: ID
+    callback('error', null);
+  });
+
+  return service.signUp('testuser', 'password', [], null).then(() => {
+    assert.ok(false); // shouldn't get here.
+  }).catch((err) => {
+    assert.equal(err, 'error');
+  });
 });
