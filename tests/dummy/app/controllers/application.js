28d69b631f1efe1f5700c3f68ad16889ed360943
Paul Watts <paulcwatts@gmail.com>
2020-08-19 15:15:11 -0700

    Begin updating syntax
    I know we can update to angle bracket components.
    Can we also update to the {{fn}} helper?

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,17 +1,17 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { set } from '@ember/object';
+import { action, set } from '@ember/object';
 import { alias } from '@ember/object/computed';
 
-export default Controller.extend({
-  cognito: service(),
-  currentUser: service(),
-  session: service(),
-  poolId: alias('cognito.poolId'),
-  clientId: alias('cognito.clientId'),
+export default class ApplicationController extends Controller {
+  @service cognito;
+  @service currentUser;
+  @service session;
+  @alias('cognito.poolId') poolId;
+  @alias('cognito.clientId') clientId;
 
-  init() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
     let poolId = localStorage.getItem('ember-cognito/dummy/poolId');
     let clientId = localStorage.getItem('ember-cognito/dummy/clientId');
     // Ignore the defaults, which are used to test the configuration in unit tests
@@ -22,17 +22,17 @@ export default Controller.extend({
     }
     set(this, 'poolId', poolId);
     set(this, 'clientId', clientId);
-  },
-
-  actions: {
-    saveIds(e) {
-      e.preventDefault();
-      localStorage.setItem('ember-cognito/dummy/poolId', this.poolId);
-      localStorage.setItem('ember-cognito/dummy/clientId', this.clientId);
-    },
-
-    logout() {
-      this.session.invalidate();
-    }
   }
-});
+
+  @action
+  saveIds(e) {
+    e.preventDefault();
+    localStorage.setItem('ember-cognito/dummy/poolId', this.poolId);
+    localStorage.setItem('ember-cognito/dummy/clientId', this.clientId);
+  }
+
+  @action
+  logout() {
+    this.session.invalidate();
+  }
+}
