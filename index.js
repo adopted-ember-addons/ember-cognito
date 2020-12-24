e9f8e260fab3a2699080b98c2afb0d961e6b0517
Robert Wagner <rwwagner90@gmail.com>
2020-12-23 22:58:47 -0500

    Possible fix for crypto

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 
 module.exports = {
   name: require('./package').name,
+  options: {
+    autoImport:{
+      webpack: {
+        node: { crypto: true }
+      }
+    }
+  },
   included: function(app) {
     this._super.included.apply(this, arguments);
     app.import('vendor/shims/window-global.js');
