5a3e2ee0d6d6dd8c23bcf9e61e4fe7cc31f7ea10
Robert Wagner <rwwagner90@gmail.com>
2025-08-19 08:14:59 -0400

    Get tests passing

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,4 +6,20 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('attribute');
+  this.route('attribute-verify');
+  this.route('change-password');
+  this.route('delete-user');
+  this.route('forgot-password', function () {
+    this.route('confirm');
+  });
+  this.route('login', function () {
+    this.route('new-password');
+  });
+  this.route('profile');
+  this.route('register', function () {
+    this.route('confirm');
+    this.route('resend');
+  });
+});
